1589ca52dd72855c669b053726f6fcb62f5bfa25
Karl <kkarjamaa@gmail.com>
2018-03-05 19:48:04 +0200

    Spring Boot Backend
    Build path'i jäi minig error sisse

---
+++ back/CoachMeSpring/src/main/java/Delta/CoachMeSpring/CoachMeSpringApplication.java
@@ -0,0 +1,12 @@
+package Delta.CoachMeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachMeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachMeSpringApplication.class, args);
+	}
+}
