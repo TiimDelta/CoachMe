815339ed53b18af270390e78399c5b6b22e4e5c9
rahind <raikohindpere@gmail.com>
2018-03-12 19:54:23 +0200

    Database half done
    Database peaaegu küljes. Jätkan hiljem

--- back/CoachMeSpring/src/main/java/Delta/CoachMeSpring/Training.java
+++ back/CoachMeSpring/src/main/java/Delta/CoachMeSpring/Training.java
@@ -1,10 +1,20 @@
 package Delta.CoachMeSpring;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 
 @Entity
+@Getter
+@Setter
 public class Training {
+    @Id
+    @GeneratedValue
+    Integer Id;
     String trainerName;
     String location;
     String eMail;
