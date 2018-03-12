3c85d9b222372669ce3bae9579a6cbd3ae8ab13f
rahind <raikohindpere@gmail.com>
2018-03-12 18:50:15 +0200

    Gradle korda
    Erinevad compiled, et gradle buildi jooksutades impordid toimiks.

--- back/CoachMeSpring/src/main/java/Delta/CoachMeSpring/TrainingRepository.java
+++ back/CoachMeSpring/src/main/java/Delta/CoachMeSpring/TrainingRepository.java
@@ -1,10 +1,11 @@
 package Delta.CoachMeSpring;
 
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrainingRepository extends CrudRepository<Training, Long>{
+public interface TrainingRepository extends CrudRepository<Training, Long> {
 
 }
 
