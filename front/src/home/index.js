f31495ff621ed929dcd076c98c3523cc9aa17c02
rahind <raikohindpere@gmail.com>
2018-03-13 21:56:00 +0200

    Fetch client in action
    front-endist saab saata päringu backendi mis suhtleb siis databaseiga.
    
    Suhelda saavad aga error 500 tuleb, seega databasei ei salvesta.

--- front/src/home/index.js
+++ front/src/home/index.js
@@ -1,3 +1,26 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
 export class Home{
 
+	addTraining(){
+		let client = new HttpClient();
+
+		let trainingData = {
+      "trainer": "xxTommy",
+    	"location": "Idaxx",
+    	"mail": "axx",
+    	"workout": "Jomm"
+		}
+
+		client.fetch('http://localhost:8080/trainings/add', {
+			'method': "POST",
+			'body': json(trainingData)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Server saatis " + data.trainer);
+		});
+
+		console.log("Method executed!")
+	}
 }
