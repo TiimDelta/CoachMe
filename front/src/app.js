0b020212f7778a74dcba029f477bee5c715d9952
Karlos Heinla <karlos_heinla@hotmail.com>
2018-03-23 16:17:27 +0200

    Front-end muudatused
    Eraldi leht treenerite jaoks. Navabaril nüüd treenerid ja meist lingid.

--- front/src/app.js
+++ front/src/app.js
@@ -7,6 +7,7 @@ export class App {
     config.title = 'My Aurelia app';
     config.map([
       { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
+      { route: 'coach',       name: 'coach',       moduleId: 'resources/coach', nav:true }
     ]);
   }
 }
---
+++ front/src/resources/coach.js
@@ -0,0 +1,38 @@
+import {HttpClient, json} from 'aurelia-fetch-client'
+
+export class coach{
+
+  trainingData ={ "trainer" : "",
+    "location":"",
+    "workout":"",
+    "mail":""
+  }
+  trainingList =[]
+
+  activate(){
+    let client = new HttpClient();
+    client.fetch('http://localhost:8080/trainings')
+      .then(response => response.json())
+      .then(trainings => this.trainingList = trainings);
+
+    console.log("Get Method executed!")
+  }
+  addTraining(){
+    this.activate();
+    let client = new HttpClient();
+
+
+
+    client.fetch('http://localhost:8080/trainings/add', {
+      'method': "POST",
+      'body': json(this.trainingData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Server saatis " + data.trainer);
+      });
+
+    console.log("Method executed!");
+
+  }
+}
